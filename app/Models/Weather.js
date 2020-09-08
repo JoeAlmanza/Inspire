c9708c96b6c4fa19f281d3bacfae35a7f6fb7ea1
Joe Almanza <joealmanza2113@gmail.com>
2020-09-08 08:54:57 -0600

    trying to get GitHub to work

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -1,6 +1,5 @@
 export default class Weather {
   constructor(data) {
-
     this.city = data.name
     this.kelvin = data.main.temp
     this.celcius = Math.floor(this.kelvin - 273.15) + "&deg; C"
@@ -9,11 +8,8 @@ export default class Weather {
     console.log('[RAW WEATHER API DATA]', data); 
     //XX NOTE Have you ever wanted to know the temperature measured in kelvin? 
     //      That is what this data returns! data.main.temp is the temperature in Kelvin
-
-
     //TODO You should convert the temperature data to either F or C
     //      check out the other data that comes back and see if there is anything you want to try
-
   }
 
   get fTemplate() {
